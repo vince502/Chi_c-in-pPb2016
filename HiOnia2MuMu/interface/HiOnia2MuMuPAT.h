b51dda2ce48fa3bf02b4f78632115d62bb9a3273
soohwan lee <62940130+vince502@users.noreply.github.com>
2023-11-26 00:11:56 +0900

    up

--- HiOnia2MuMu/interface/HiOnia2MuMuPAT.h
+++ HiOnia2MuMu/interface/HiOnia2MuMuPAT.h
@@ -7,8 +7,8 @@
 
 // FW include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/stream/EDProducer.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "CommonTools/Utils/interface/PtComparator.h"
@@ -44,7 +44,7 @@ struct GreaterByVProb {
 // class declaration
 //
 
-class HiOnia2MuMuPAT : public edm::EDProducer {
+class HiOnia2MuMuPAT : public edm::stream::EDProducer<> {
   public:
     explicit HiOnia2MuMuPAT(const edm::ParameterSet&);
     ~HiOnia2MuMuPAT();
