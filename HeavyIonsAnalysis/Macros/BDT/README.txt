799c593941e295d092e46b6c92ebd7bbb270f340
soohwan lee <62940130+vince502@users.noreply.github.com>
2023-11-25 23:57:27 +0900

    up

--- HeavyIonsAnalysis/Macros/BDT/README.txt
+++
@@ -1,2 +0,0 @@
-The code has to be run with ROOT 6.12 and not 6.06
-For example CMSSW_10_3_2 has the right version.
--- HeavyIonsAnalysis/Macros/Fitter/Systematics/CondorFiles/condorExecJpsi.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /eos/cms/store/group/phys_heavyions/okukral/Chi_c/Workspace/CMSSW_8_0_30/src/HeavyIonsAnalysis/Macros/Fitter/Systematics/
-cmsenv
-root -l -b -q 'SystFits_Jpsi.C('$1', '$2')' 
